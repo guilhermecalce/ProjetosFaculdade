66d3cfb3ad64d7477124f6c79ad3dd767d4e807b
Guilherme Calce da Silva <47507301+guilhermecalce@users.noreply.github.com>
2019-03-04 16:44:04 -0300

    Add files via upload

---
+++ NomeMaior.py
@@ -0,0 +1,9 @@
+#3) Faça um programa que coloque dois nomes em ordem alfabética.
+nome1 = input ("Digite um nome : ")
+nome2 = input ("Digite outro nome : ")
+if nome1 > nome2:
+ print("O nome que vem primeiro é o nome do(a) : ", nome2)
+ print ("O nome que vem depois é o nome do(a) : ", nome1)
+else:
+    print("O nome que vem primeiro é o nome do(a) : ", nome1)
+    print("O nome que vem depois é o nome do(a) : ", nome2)
